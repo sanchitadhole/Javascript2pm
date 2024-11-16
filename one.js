0fd1a79d0f50ca3e2b10b3d546b7c64920476d7e
Sanchita Dhole <sanchitadhole27@gmail.com>
2024-11-16 15:09:30 +0530

    first commit

---
+++ one.js
@@ -0,0 +1,69 @@
+console.log("helloo world")
+
+
+//variable - var , let , const
+
+//var
+
+var a=1
+console.log(a)
+console.log(typeof(a));
+
+
+
+var b = "sanchita"
+console.log(typeof(b));
+
+
+var c = true
+console.log(!typeof(c))
+   
+//null
+//undefind
+
+let d = null
+console.log(typeof(d));
+
+d = "rohini"
+console.log(d)
+
+//by using var we update value
+//we cannot redlare variable with let keyword bit possible with var
+//const cannot allow reassiging also.
+
+const r = "gauri"
+console.log(r);
+
+r = "rohi"
+console.log(r);
+
+
+//  ctr + /
+
+// this is my laptop
+
+
+/* this is my laptop
+ this is my laptop
+ this is my laptop
+ this is my laptop
+ this is my laptop
+ this is my laptop
+ this is my laptop
+ this is my laptop*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
