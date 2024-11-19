a628a22e7625158799b70cccf94e11bfa7a2c708
Sanchita Dhole <sanchitadhole27@gmail.com>
2024-11-19 16:08:35 +0530

    second

---
+++ basic2.js
@@ -0,0 +1,17 @@
+let required = true
+
+if(!required){
+    console.log("condition satisfied");
+}else{
+    console.log("condition not satisfied");
+}
+
+
+
+let a = 10;
+if(a==11){
+    console.log("succesfull")
+}else{
+    console.log("not successful")
+}
+
